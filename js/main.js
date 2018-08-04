c9c42a7d1db3eefe2cba6b83daf19a66c913a62c
Sam Jackson <sammyj@gmail.com>
2018-08-04 17:00:55 -0500

    mockup site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Open Mobile Menu
+document.getElementById("openMenu").onclick = function() {
+    document.getElementById("mobile-overlay").style.width = "100vw";
+  }
+//Close Mobile Menu
+document.getElementById("closeMenu").onclick = function() {
+    document.getElementById("mobile-overlay").style.width = "0vw";
+}
